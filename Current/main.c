7cd3e7310357b8f8e1d82fa2d34e70e4c50d0431
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2026-03-10 19:02:23 +0100

    exercises 1-4 from 4_GPIO

--- Current/main.c
+++ Current/main.c
@@ -1,6 +1,21 @@
 #include <LPC21xx.H>
 
-int main() {
+void Delay() {
+	
+	unsigned int uiDelayCounter;
+	
+	for(uiDelayCounter = 0; uiDelayCounter < 7500750; uiDelayCounter++) {};  
+}
 
+int main() {
+	
+	IO1DIR = 0x00010000;
+	
+	while(1) {
+			IO1SET = 0x00010000;
+			Delay();
+			IO1CLR = 0x00010000;
+	}
+	
 	return 0;
 }
